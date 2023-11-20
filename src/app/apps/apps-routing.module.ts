47b1772895337d9f2f6ed074ef7365d9bf194f2d
Pavel Salauyou <pavel.salauyou@gmail.com>
2023-11-20 16:28:32 +0300

    fix favicon app

--- src/app/apps/apps-routing.module.ts
+++ src/app/apps/apps-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // {
-  //   path: 'favicon-generator',
-  //   loadComponent: () => import('./favicon-generator/favicon-generator.component').then(c => c.FaviconGeneratorComponent),
-  //   data: {
-  //     breadcrumb: 'Favicon Generator'
-  //   }
-  // },
+  {
+    path: 'favicon-generator',
+    loadComponent: () => import('./favicon-generator/favicon-generator.component').then(c => c.FaviconGeneratorComponent),
+    data: {
+      breadcrumb: 'Favicon Generator'
+    }
+  },
   {
     path: 'monaco-editor',
     loadComponent: () => import('./monaco-editor/monaco-editor.component').then(c => c.MonacoEditorComponent),
